d7371463661f3b445b4a27b555963571a69d061e
Nirmal Raj (HLB) <NirmalRaj@hlbb.hongleong.com.my>
2020-12-20 16:54:34 +0800

    include search component and testing libraries

---
+++ src/UI/search/search.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+
+import Button from '../button/button';
+import './search.css';
+
+const search = (props) => {
+
+    const onSearch = () => {
+        let id = document.getElementById('searchText').value;
+        props.onSearch(id);
+    }
+
+    return (
+        <Fragment>
+            <input type='text' placeholder='enter id to search' id='searchText' className='inputBox'>
+            </input>
+            <Button label='Search' clickHandler={onSearch}></Button>
+        </Fragment>
+    );
+}
+export default search;
